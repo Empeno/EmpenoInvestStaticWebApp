2d4af44c1173b16044a75d778158ff5e41fe599a
Marc Made It <spurdo.dk@gmail.com>
2025-01-15 09:25:04 +0100

    Update Analytics.tsx

--- client/src/pages/Analytics.tsx
+++ client/src/pages/Analytics.tsx
@@ -1,7 +1,12 @@
 import { useEffect, useState } from 'react';
 
+interface Data {
+  key1: string;
+  key2: string;
+}
+
 const Analytics = () => {
-  const [data, setData] = useState('');
+  const [data, setData] = useState<Data | null>(null);
   const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
@@ -12,8 +17,8 @@ const Analytics = () => {
           throw new Error(`HTTP error! status: ${response.status}`);
         }
         console.log('Response:', response);
-        const { text } = await response.json();
-        setData(text);
+        const jsonData: Data = await response.json();
+        setData(jsonData);
       } catch (error) {
         console.error('Error fetching data:', error);
         setError(error instanceof Error ? error.message : 'Unknown error');
@@ -23,11 +28,11 @@ const Analytics = () => {
 
   return (
     <div className="flex flex-col gap-3 md:gap-5">
-      <div className="w-full h-96 bg-base-100 rounded-lg shadow-md"></div>
-      <div className="w-full h-96 bg-base-100 rounded-lg shadow-md"></div>
       <div className="w-full h-96 bg-base-100 rounded-lg shadow-md">
-        {error ? <div>Error: {error}</div> : <div>{data}</div>}
+        {error ? <div>Error: {error}</div> : <div>{data?.key1}</div>}
       </div>
+      <div className="w-full h-96 bg-base-100 rounded-lg shadow-md"></div>
+      <div className="w-full h-96 bg-base-100 rounded-lg shadow-md"></div>
     </div>
   );
 };
