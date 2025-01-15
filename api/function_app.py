2f23661ead4619ae474000f8cb65c2cece7b713a
Morten Empeño <morten@empeno.dk>
2025-01-15 09:12:28 +0100

    return json instead of text

--- api/function_app.py
+++ api/function_app.py
@@ -1,25 +1,58 @@
 import azure.functions as func
 import logging
+import json
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
 @app.route(route="http_trigger")
 def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
-
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
-
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    else:
+    try:        
+        response_data = {
+            "message": "Hello, Azure Functions!",
+            "success": True,
+            "data": {
+                "key1": "value1",
+                "key2": "value2",
+                "danskebank": "103"
+            }
+        }
+        
+        response_json = json.dumps(response_data)
+                
         return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
+            response_json,
+            status_code=200,
+            mimetype="application/json"
         )
+    except Exception as e:
+        error_message = {
+            "error": str(e)
+        }
+        return func.HttpResponse(
+            json.dumps(error_message),
+            status_code=500,
+            mimetype="application/json"
+        )
+
+
+
+
+
+    # logging.info('Python HTTP trigger function processed a request.')
+
+    # name = req.params.get('name')
+    # if not name:
+    #     try:
+    #         req_body = req.get_json()
+    #     except ValueError:
+    #         pass
+    #     else:
+    #         name = req_body.get('name')
+
+    # if name:
+    #     return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    # else:
+    #     return func.HttpResponse(
+    #          "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+    #          status_code=200
+    #     )
