e8b4389187eec16fab9beb521bf619497a4b7177
Marc Made It <spurdo.dk@gmail.com>
2025-01-24 15:05:46 +0100

    Update UploadManuel.tsx

--- client/src/components/admin/data/components/UploadManuel.tsx
+++ client/src/components/admin/data/components/UploadManuel.tsx
@@ -15,18 +15,16 @@ const UploadManuel = () => {
 
   return (
     <div className="flex flex-col gap-5 ">
-      <form onSubmit={handleCreateManual} className="flex flex-col gap-5">
-        <SelectIndustry onIndustryCreated={handleIndustryCreated} />
+      <SelectIndustry onIndustryCreated={handleIndustryCreated} />
 
-        <input
-          type="text"
-          placeholder="Enter data manually"
-          className="input input-bordered"
-        />
-        <button type="submit" className="btn btn-primary">
-          Create data
-        </button>
-      </form>
+      <input
+        type="text"
+        placeholder="Enter data manually"
+        className="input input-bordered"
+      />
+      <button type="submit" className="btn btn-primary">
+        Create data
+      </button>
 
       {showToast && (
         <div className="toast toast-end">
