675729ed64a7b85c167104641ed5682db4619516
Morten Empeño <morten@empeno.dk>
2025-02-07 08:14:57 +0100

    using identity for db table

--- db-script/Create_Table_Industries.sql
+++ db-script/Create_Table_Industries.sql
@@ -1,6 +1,12 @@
+-- Create a new table called '[Industries]' in schema '[dbo]'
+-- Drop the table if it already exists
+IF OBJECT_ID('[dbo].[Industries]', 'U') IS NOT NULL
+DROP TABLE [dbo].[Industries]
+GO
+-- Create the table in the specified schema
 CREATE TABLE [dbo].[Industries]
 (
-    [Id] UNIQUEIDENTIFIER DEFAULT NEWID() PRIMARY KEY, 
+    [Id] INT IDENTITY(1,1) PRIMARY KEY,
     [Name] NVARCHAR(50) NOT NULL,
     [Description] NVARCHAR(300) NOT NULL
 );
