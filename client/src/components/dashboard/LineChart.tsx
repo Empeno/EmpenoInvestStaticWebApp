6178b3c351cf9f4443355054c502967f6ddeb104
Marc Made It <spurdo.dk@gmail.com>
2025-03-25 15:58:26 +0100

    Update LineChart.tsx

--- client/src/components/dashboard/LineChart.tsx
+++ client/src/components/dashboard/LineChart.tsx
@@ -1,28 +1,55 @@
-import React from "react";
-import { Line } from "react-chartjs-2";
-import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend, ChartData, ChartOptions } from "chart.js";
+import React from 'react';
+import { Line } from 'react-chartjs-2';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+  ChartData,
+  ChartOptions,
+} from 'chart.js';
 
 interface LineProps {
-  options: ChartOptions<"line">;
-  data: ChartData<"line">;
+  options: ChartOptions<'line'>;
+  data: ChartData<'line'>;
 }
 
-ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend);
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+);
 
 const LineChart: React.FC<LineProps> = ({ data, options }) => {
   return <Line data={data} options={options} />;
 };
 
 const ExampleUsage = () => {
-  const labels = ["January", "February", "March", "April", "May", "June", "July"];
+  const labels = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+  ];
   const data = {
     labels: labels,
     datasets: [
       {
-        label: "Total Profit",
+        label: 'Total Profit',
         data: [855, 755, 956, 841, 690, 750, 950],
         fill: false,
-        borderColor: "rgb(56, 189, 248)",
+        borderColor: 'rgb(56, 189, 248)',
         tension: 0.1,
         borderWidth: 2,
       },
@@ -33,11 +60,11 @@ const ExampleUsage = () => {
     responsive: true,
     plugins: {
       legend: {
-        position: "top" as const,
+        position: 'top' as const,
       },
       title: {
         display: true,
-        text: "Last 7 months",
+        text: 'Last 7 months',
       },
     },
   };
